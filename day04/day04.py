f4fba42ffd637ef76fdbaa778499e6c131cb57ef
Mark Wanders <mndmwanders@gmail.com>
2022-12-04 10:35:54 +0100

    Refactored day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,13 +1,15 @@
+import re
+
 with open("input.txt") as f:
     pairs = f.read().splitlines()
 
 counter1 = 0
 counter2 = 0
 for pair in pairs:
-    pair1, pair2, = [[int(x.split("-")[0]), int(x.split("-")[-1])] for x in pair.split(",")]
-    if (pair1[0] >= pair2[0] and pair1[-1] <= pair2[-1]) or (pair2[0] >= pair1[0] and pair2[-1] <= pair1[-1]):
+    a, b, c, d, = map(int, re.findall(r'\d+', pair))
+    if (a >= c and b <= d) or (c >= a and d <= b):
         counter1 += 1
-    if pair2[0] <= pair1[0] <= pair2[-1] or pair2[0] <= pair1[-1] <= pair2[-1] or pair1[0] <= pair2[0] <= pair1[-1] or pair1[0] <= pair2[-1] <= pair1[-1]:
+    if c <= a <= d or c <= b <= d or a <= c <= b or a <= d <= b:
         counter2 += 1
 print(counter1)
 print(counter2)
