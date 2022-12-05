78e8e6f0a838fc8ef275264c154aa5815c7dbb00
Mark Wanders <mndmwanders@gmail.com>
2022-12-05 10:12:01 +0100

    Day 5 solved part 2

--- day05/day05.py
+++ day05/day05.py
@@ -5,16 +5,23 @@ with open("input.txt") as f:
     crates = crates.split("\n")
     moves = moves.split("\n")
 
-stacks = [[]] * 9
+stacks1 = [[]] * 9
+stacks2 = [[]] * 9
 for crate in crates:
     row = [crate[i:i+4] for i in range(0, len(crate), 4)]
     for i, c in enumerate(row):
         if '[' in c:
-            stacks[i] = [c[1]] + stacks[i]
-print(stacks)
+            stacks1[i] = [c[1]] + stacks1[i]
+            stacks2[i] = [c[1]] + stacks2[i]
+print(stacks1)
 for move in moves:
     n, s, t = map(int, re.findall(r'\d+', move))
     print(n, s, t)
     for i in range(n):
-        stacks[t - 1] = stacks[t - 1] + [stacks[s - 1].pop()]
-print("".join([s for s in [(stack or [None])[-1] for stack in stacks] if s is not None]))
+        stacks1[t - 1] = stacks1[t - 1] + [stacks1[s - 1].pop()]
+    stacks2[t - 1] = stacks2[t - 1] + stacks2[s - 1][-n:]
+    stacks2[s - 1] = stacks2[s - 1][:-n]
+
+print("".join([s for s in [(stack or [None])[-1] for stack in stacks1] if s is not None]))
+print("".join([s for s in [(stack or [None])[-1] for stack in stacks2] if s is not None]))
+print(stacks2)
