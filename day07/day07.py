dca14ac277f31a03fa0127f9af0fdc27a0432966
Mark Wanders <mndmwanders@gmail.com>
2022-12-07 10:04:56 +0100

    Solved day 7 part 1

---
+++ day07/day07.py
@@ -0,0 +1,26 @@
+with open("input.txt") as f:
+    lines = f.read().split("\n")
+
+dirs = {}
+dir_path = []
+for line in lines:
+    parts = line.split(" ")
+    if parts[0] == "$":
+        if parts[1] == "cd":
+            if parts[2] == "..":
+                dir_path.pop()
+            else:
+                if parts[2] == "/":
+                    pwd = parts[2]
+                else:
+                    pwd = dir_path[-1] + parts[2] + "/"
+                dir_path.append(pwd)
+                if pwd not in dirs.keys():
+                    dirs[pwd] = 0
+    elif parts[0] != "dir":
+        size = int(parts[0])
+        for d in dir_path:
+            dirs[d] = dirs[d] + size
+print(sum(v for v in dirs.values() if v <= 100000))
+
+
