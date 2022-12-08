0d02747701eb092f33c4aa4f9f2284f7c69f7112
markwa <mark.wanders@adyen.com>
2022-12-08 15:52:40 +0100

    Refactored day 6

--- day06/day06.py
+++ day06/day06.py
@@ -1,13 +1,13 @@
 with open("input.txt") as f:
     stream = f.readline()
 
-for i in range(4, len(stream)):
-    buffer = stream[i-4:i]
-    if len(set(buffer)) == 4:
-        print(i)
-        break
-for i in range(14, len(stream)):
-    buffer = stream[i-14:i]
-    if len(set(buffer)) == 14:
-        print(i)
-        break
+
+def solve(size):
+    for i in range(size, len(stream)):
+        buffer = stream[i - size:i]
+        if len(set(buffer)) == size:
+            return i
+
+
+print(solve(4))
+print(solve(14))
