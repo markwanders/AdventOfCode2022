d7a760fac1ca688916ac82354bf0a8b84072ee2b
markwa <mark.wanders@adyen.com>
2022-12-19 12:49:11 +0100

    Day 16 solved part 2

--- day16/day16.py
+++ day16/day16.py
@@ -36,3 +36,41 @@ def dfs(node, opened, rate, time):
 
 
 print(dfs("AA", [], 0, 30))
+cache = {}
+
+
+def dfs2(node, elephant, opened, rate, time):
+    if time == 0:
+        return 0
+    if (rate, time, node, elephant) in cache.keys():
+        return cache[(rate, time, node, elephant)]
+    released = 0
+
+    node_open = node not in opened and valves[node].flow > 0
+    elephant_open = elephant not in opened and valves[elephant].flow > 0 and node != elephant
+
+    if node_open:
+        if elephant_open:
+            next_opened = opened + [node] + [elephant]
+            next_rate = rate + valves[node].flow + valves[elephant].flow
+            released = dfs2(node, elephant, next_opened, next_rate, time - 1)
+        else:
+            next_opened = opened + [node]
+            next_rate = rate + valves[node].flow
+            for a in valves[elephant].adjacent:
+                released = max(released, dfs2(node, a, next_opened, next_rate, time - 1))
+    else:
+        if elephant_open:
+            next_opened = opened + [elephant]
+            next_rate = rate + valves[elephant].flow
+            for a in valves[node].adjacent:
+                released = max(released, dfs2(a, elephant, next_opened, next_rate, time - 1))
+    for a in valves[node].adjacent:
+        for b in valves[elephant].adjacent:
+            released = max(released, dfs2(a, b, opened, rate, time - 1))
+    released += rate
+    cache[(rate, time, node, elephant)] = released
+    return released
+
+
+print(dfs2("AA", "AA", [], 0, 26))
