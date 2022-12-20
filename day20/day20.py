87e9a52e5c6d7f3ee394f0f28aaa8087cf9b8535
Mark Wanders <mndmwanders@gmail.com>
2022-12-20 10:41:20 +0100

    Solved day 20 part 2, slow but correct!

--- day20/day20.py
+++ day20/day20.py
@@ -9,23 +9,26 @@ class Number:
 
 
 with open("input.txt") as f:
-    nums = [Number(int(n), i) for i, n in enumerate(f.read().split("\n"))]
+    nums = [Number(int(n) * 811589153, i) for i, n in enumerate(f.read().split("\n"))]
 
-for num in nums:
-    start, finish = num.index, (num.index + num.value) % (len(nums) - 1)
-    if finish == 0:
-        finish = len(nums) - 1
-    # print("Should move %i to from position %i to %i" % (num.value, start, finish))
-    nums_to_move = [n for n in nums if n.index in range(min(start, finish), max(start, finish) + 1) and n != num]
-    # print(nums_to_move)
-    for num_to_move in nums_to_move:
-        if start < finish:
-            num_to_move.index = num_to_move.index - 1
-        else:
-            num_to_move.index = num_to_move.index + 1
-    num.index = finish
-    # print(sorted(nums, key=lambda n: n.index))
-    # print([n.index for n in sorted(nums, key=lambda n: n.index)])
+i = 10
+while i > 0:
+    for num in nums:
+        start, finish = num.index, (num.index + num.value) % (len(nums) - 1)
+        if finish == 0:
+            finish = len(nums) - 1
+        # print("Should move %i to from position %i to %i" % (num.value, start, finish))
+        nums_to_move = [n for n in nums if n.index in range(min(start, finish), max(start, finish) + 1) and n != num]
+        # print(nums_to_move)
+        for num_to_move in nums_to_move:
+            if start < finish:
+                num_to_move.index = num_to_move.index - 1
+            else:
+                num_to_move.index = num_to_move.index + 1
+        num.index = finish
+        # print(sorted(nums, key=lambda n: n.index))
+        # print([n.index for n in sorted(nums, key=lambda n: n.index)])
+    i -= 1
 index_zero = [n.index for n in nums if n.value == 0][0]
 part_1 = 0
 for c in range(1000, 4000, 1000):
