88affcc44d80bf6133857b7a0ad0d78b6be22ada
Mark Wanders <mndmwanders@gmail.com>
2023-01-02 12:03:46 +0100

    Solved day 24 part 1

---
+++ day24/day24.py
@@ -0,0 +1,56 @@
+with open("input.txt") as f:
+    lines = f.read().split("\n")
+
+grid = set()
+
+
+class Blizzard:
+    def __init__(self, initial_position, direction):
+        self.position = initial_position
+        self.direction = direction
+
+    def move(self):
+        if self.direction == "<":
+            self.position -= 1
+            if self.position not in pos:
+                self.position = complex(max(p.real for p in pos), self.position.imag)
+        elif self.direction == ">":
+            self.position += 1
+            if self.position not in pos:
+                self.position = complex(min(p.real for p in pos), self.position.imag)
+        elif self.direction == "^":
+            self.position -= 1j
+            if self.position not in pos:
+                self.position = complex(self.position.real, max(p.imag for p in pos))
+        elif self.direction == "v":
+            self.position += 1j
+            if self.position not in pos:
+                self.position = complex(self.position.real, min(p.imag for p in pos))
+        return self
+
+
+blizzards = []
+start = 0
+finish = 0
+
+for y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        if char in ['<', '>', '^', 'v']:
+            blizzards.append(Blizzard(complex(x, y), char))
+            grid.add(complex(x, y))
+        elif char == '.':
+            if start == 0:
+                start = complex(x, y)
+            else:
+                finish = complex(x, y)
+            grid.add(complex(x, y))
+
+pos = grid - {start, finish}
+t = 0
+queue = {start}
+while finish not in queue:
+    t += 1
+    blizzards = [b.move() for b in blizzards]
+    neighbours = {n + d for d in (1, -1, 1j, -1j, 0) for n in queue if n in grid}
+    queue = neighbours - {b.position for b in blizzards}
+print(t)
